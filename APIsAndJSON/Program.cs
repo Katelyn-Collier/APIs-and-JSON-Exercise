21b097e00f92afc1bc4735d9cdde2c4a4e1dd78e
Katelyn-Collier <katelyncollier14@gmail.com>
2023-04-05 13:07:45 -0500

    Exercise 1

--- APIsAndJSON/Program.cs
+++ APIsAndJSON/Program.cs
@@ -1,10 +1,18 @@
-﻿namespace APIsAndJSON
+﻿using System;
+using System.Net.Http;
+using Newtonsoft.Json.Linq;
+
+namespace APIsAndJSON
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            for (int i = 0; i < 5; i++)
+            {
+                QuoteGen.KanyeQuote();
+                QuoteGen.RonQuote();
+            }
         }
     }
 }
